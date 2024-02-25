8a118d60a1f284a0b4db9b26ed3e11766738792a
Nimrod Gal <gal.nimrod@gmail.com>
2024-02-25 20:00:32 +0200

    controllers and folders

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log("works");
+import express from 'express'
+import morgan from 'morgan';
+import * as dotenv from 'dotenv'
+const app = express()
+dotenv.config();
+
+
+//routers
+import jobRouter from './routes/jobRouter.js'
+
+if(process.env.NODE_ENV ==='development'){
+    app.use(morgan('dev'));
+}
+app.use(express.json());
+
+app.get('/', (req,res)=>{res.send('hello world')});
+app.post('/',(req,res)=>{
+    console.log(req);
+    res.json({message:"data recieved", data:req.body})
+})
+
+app.use('/api/v1/jobs', jobRouter);
+
+app.use('*', (req,res)=>{
+    res.status(404).json({msg:'not found'})
+})
+app.use((err,req,res,next)=>{
+    console.log(err);
+    res.status(500).json({msg:'something went wrong'})
+})
+const port =process.env.PORT || 5100
+
+app.listen(port,()=>{console.log(`server is running on PORT ${port}`);});
