021478f4cf4d66ffcc907f0b36da07781e0f4ac3
Nimrod Gal <gal.nimrod@gmail.com>
2024-02-28 22:24:06 +0200

    status codes and custom errors

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,53 +1,39 @@
 import Job from '../models/JobModel.js';
-import { nanoid } from 'nanoid';
+import {StatusCodes} from 'http-status-codes'
+import { NotFoundError } from '../errors/customErrors.js';
+import { error } from 'console';
 
-let jobs = [
-    {id:nanoid(), company:'apple', position:'front-end'},
-    {id:nanoid(), company:'google', position:'back-end'},
-
-]
 export const getAllJobs = async (req,res)=>{
-    res.status(200).json({jobs})
+    const jobs = await Job.find({})
+    res.status(StatusCodes.OK).json({jobs})
 };
 
 export const createJob = async (req,res)=>{
-    const {company,position} = req.body
-    const job = await Job.create({
-        company,
-        position
-    })
-    res.status(201).json({job})
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
 }
 export const getJob = async (req,res)=>{
     const {id} = req.params
-    const job = jobs.find((job)=>job.id === id)
+    const job = await Job.findById(id)
     if(!job){
-        return res.status(404).json({msg:`no job with id ${id}`})
+        throw new NotFoundError(`no job with id ${id}`)
     }
-    res.status(200).json(job)
+    res.status(StatusCodes.OK).json(job)
 }
 export const updateJob = async (req,res)=>{
-    const {company,position} =req.body;
-    if(!company || !position){
-        res.status(404).json({msg:'please provide all values'})
-        return
-    }
     const {id} = req.params
-    const job = jobs.find((job)=> job.id === id)
-    if(!job){
-        return res.status(404).json({msg: `no job with id ${id}`})
+
+    const updatedJob = await Job.findByIdAndUpdate(id, req.body, {new:true})
+    if(!updatedJob){
+        throw new NotFoundError(`no job with id ${id}`)
     }
-    job.company = company;
-    job.position = position
-    res.status(200).json({msg:'job midified',job})
+    res.status(StatusCodes.OK).json({msg:'job midified',job:updatedJob})
 }
 export const deleteJob = async (req,res)=>{
     const {id} = req.params;
-    const job = jobs.find((job)=>job.id === id);
-    if(!job){
-        return req.status(404).json({msg: `no job with id ${id}`})
+    const removedJob = await Job.findByIdAndDelete(id);
+    if(!removedJob){
+        throw new NotFoundError(`no job with id ${id}`)
     }
-    const newJobs = jobs.filter((job)=>job.id !== id)
-    jobs = newJobs;
-    res.status(200).json({msg:"job deleted"})
+    res.status(StatusCodes.OK).json({msg:"job deleted", job:removedJob})
 }
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import 'express-async-errors'
 import express from 'express'
 import morgan from 'morgan';
 import mongoose from 'mongoose';
@@ -8,6 +9,9 @@ dotenv.config();
 
 //routers
 import jobRouter from './routes/jobRouter.js'
+
+//middleware
+import errorHandlermiddleware from './middleware/errorHandlerMiddleware.js';
 
 if(process.env.NODE_ENV ==='development'){
     app.use(morgan('dev'));
@@ -25,10 +29,8 @@ app.use('/api/v1/jobs', jobRouter);
 app.use('*', (req,res)=>{
     res.status(404).json({msg:'not found'})
 })
-app.use((err,req,res,next)=>{
-    console.log(err);
-    res.status(500).json({msg:'something went wrong'})
-})
+app.use(errorHandlermiddleware)
+
 const port =process.env.PORT || 5100
 try {
     await mongoose.connect(process.env.MONGO_URL)
